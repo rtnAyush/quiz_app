6b46e7b6578d8023067b4e291d9ea2b9cda3e6e3
Ayush Kumar <7126924ak@gmail.com>
2024-12-18 09:28:12 +0530

    change setting.py

--- quiz_app/settings.py
+++ quiz_app/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i1g#a$ui77dz858_iczuxr(&@fec@g66ilrc&sdk8)=nr328s3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = []
 # api/settings.py
@@ -143,12 +143,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# if DEBUG:
+# else:
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
 
 # Default primary key field type
